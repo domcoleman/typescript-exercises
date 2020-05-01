b5dc6556e3c0aba9fb734e89b6216ae499878b8f
Dom Coleman <me@domcoleman.com>
2020-05-01 07:06:03 +0100

    exercise 7

--- exercises/exercise-07/index.ts
+++ exercises/exercise-07/index.ts
@@ -1,101 +1,108 @@
-import chalk from 'chalk';
+import chalk from "chalk";
 
 /*
 
 Intro:
 
-    Project grew and we ended up in a situation with
-    some users starting to have more influence.
-    Therefore, we decided to create a new person type
-    called PowerUser which is supposed to combine
-    everything User and Admin have.
+  Project grew and we ended up in a situation with
+  some users starting to have more influence.
+  Therefore, we decided to create a new person type
+  called PowerUser which is supposed to combine
+  everything User and Admin have.
 
 Higher difficulty exercise:
 
-    Define type PowerUser which should have all fields
-    from both User and Admin (except for type),
-    and also have type 'powerUser' without duplicating
-    all the fields in the code.
+  Define type PowerUser which should have all fields
+  from both User and Admin (except for type),
+  and also have type 'powerUser' without duplicating
+  all the fields in the code.
 
 Run:
 
-    npm run 7
+  npm run 7
 
-    - OR -
+  - OR -
 
-    yarn -s 7
+  yarn -s 7
 
 */
 
 interface User {
-    type: 'user';
-    name: string;
-    age: number;
-    occupation: string;
+  type: "user";
+  name: string;
+  age: number;
+  occupation: string;
 }
 
 interface Admin {
-    type: 'admin';
-    name: string;
-    age: number;
-    role: string;
+  type: "admin";
+  name: string;
+  age: number;
+  role: string;
 }
 
-type PowerUser = unknown;
+type PowerUser = Omit<User & Admin, "type"> & { type: "powerUser" };
 
 type Person = User | Admin | PowerUser;
 
 const persons: Person[] = [
-    { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
-    { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
-    { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
-    { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' },
-    {
-        type: 'powerUser',
-        name: 'Nikki Stone',
-        age: 45,
-        role: 'Moderator',
-        occupation: 'Cat groomer'
-    }
+  {
+    type: "user",
+    name: "Max Mustermann",
+    age: 25,
+    occupation: "Chimney sweep",
+  },
+  { type: "admin", name: "Jane Doe", age: 32, role: "Administrator" },
+  { type: "user", name: "Kate Müller", age: 23, occupation: "Astronaut" },
+  { type: "admin", name: "Bruce Willis", age: 64, role: "World saver" },
+  {
+    type: "powerUser",
+    name: "Nikki Stone",
+    age: 45,
+    role: "Moderator",
+    occupation: "Cat groomer",
+  },
 ];
 
 function isAdmin(person: Person): person is Admin {
-    return person.type === 'admin';
+  return person.type === "admin";
 }
 
 function isUser(person: Person): person is User {
-    return person.type === 'user';
+  return person.type === "user";
 }
 
 function isPowerUser(person: Person): person is PowerUser {
-    return person.type === 'powerUser';
+  return person.type === "powerUser";
 }
 
 function logPerson(person: Person) {
-    let additionalInformation: string = '';
-    if (isAdmin(person)) {
-        additionalInformation = person.role;
-    }
-    if (isUser(person)) {
-        additionalInformation = person.occupation;
-    }
-    if (isPowerUser(person)) {
-        additionalInformation = `${person.role}, ${person.occupation}`;
-    }
-    console.log(`${chalk.green(person.name)}, ${person.age}, ${additionalInformation}`);
+  let additionalInformation: string = "";
+  if (isAdmin(person)) {
+    additionalInformation = person.role;
+  }
+  if (isUser(person)) {
+    additionalInformation = person.occupation;
+  }
+  if (isPowerUser(person)) {
+    additionalInformation = `${person.role}, ${person.occupation}`;
+  }
+  console.log(
+    `${chalk.green(person.name)}, ${person.age}, ${additionalInformation}`
+  );
 }
 
-console.log(chalk.yellow('Admins:'));
+console.log(chalk.yellow("Admins:"));
 persons.filter(isAdmin).forEach(logPerson);
 
 console.log();
 
-console.log(chalk.yellow('Users:'));
+console.log(chalk.yellow("Users:"));
 persons.filter(isUser).forEach(logPerson);
 
 console.log();
 
-console.log(chalk.yellow('Power users:'));
+console.log(chalk.yellow("Power users:"));
 persons.filter(isPowerUser).forEach(logPerson);
 
 // In case if you are stuck:
