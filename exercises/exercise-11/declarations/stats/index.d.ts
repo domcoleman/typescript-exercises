d93899645df1dd6a83356b2bf00fe15e3baadc93
Dom Coleman <me@domcoleman.com>
2020-05-01 07:38:04 +0100

    exercise 11

--- exercises/exercise-11/declarations/stats/index.d.ts
+++ exercises/exercise-11/declarations/stats/index.d.ts
@@ -1,3 +1,17 @@
-declare module 'stats' {
-    export function getMaxIndex(input: unknown, comparator: unknown): unknown;
+declare module "stats" {
+  type Comparator<T> = (a: T, b: T) => number;
+  type IndexFunction = <T>(input: T[], comparator: Comparator<T>) => number;
+  type ElementFunction = <T>(input: T[], comparator: Comparator<T>) => T;
+  type GetValue<T, E> = (a: T) => E;
+
+  export const getMaxIndex: IndexFunction;
+  export const getMaxElement: ElementFunction;
+  export const getMinIndex: IndexFunction;
+  export const getMinElement: ElementFunction;
+  export const getMedianIndex: IndexFunction;
+  export const getMedianElement: ElementFunction;
+  export const getAverageValue: <T, E>(
+    items: T[],
+    getValue: GetValue<T, E>
+  ) => E;
 }
